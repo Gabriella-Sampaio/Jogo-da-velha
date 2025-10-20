a279ddfba19c3ed6515f598b13edce116f1bb505
Gabriella-Sampaio <dixxgabriellasam@gmail.com>
2025-10-20 20:44:01 -0300

    layout

---
+++ script.js
@@ -0,0 +1,26 @@
+const board = document.getElementById("board");
+const cells = document.querySelectorAll(".cell");
+const statusText = document.getElementById("status");
+const restartBtn = document.getElementById("restartBtn");
+
+let currentPlayer = "X";
+let gameActive = true;
+let gameState = Array(9).fill("");
+
+const winningCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+    
+];
+
+
+
+function ReiniciaJogo(){
+
+}
